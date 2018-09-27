ef5d0899f92ac03ce8e0c8135545569117a666a0
TimothyErcia <timothy.ercia@gmail.com>
2018-09-27 12:08:38 +0800

    first commit

---
+++ CompilerMachineProblem2/CompilerMachineProblem2/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace CompilerMachineProblem2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(681, 314);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(12, 361);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(355, 175);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(618, 332);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(373, 361);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(320, 175);
+            this.richTextBox3.TabIndex = 3;
+            this.richTextBox3.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(537, 332);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(772, 12);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(383, 314);
+            this.richTextBox4.TabIndex = 5;
+            this.richTextBox4.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(699, 548);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+    }
+}
+
---
+++ CompilerMachineProblem2/CompilerMachineProblem2/Form1.cs
@@ -0,0 +1,361 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CompilerMachineProblem2
+{
+    public partial class Form1 : Form
+    {
+        string txt;
+        class VarDeclareCheck
+        {
+            public bool HasDtype(string type)
+            {
+                if (type.Equals("int") || type.Equals("string") || type.Equals("double") || type.Equals("float") || type.Equals("bool") || type.Equals("char"))
+                {
+                    return true;
+                }
+                else
+                { return false; }
+            }
+
+            public bool HasSemi(string type)
+            {
+                if(type.StartsWith("int") || type.StartsWith("string") || type.StartsWith("float") || type.StartsWith("double") || type.StartsWith("void") || type.Contains(" ") || type.StartsWith("="))
+                { return false; }
+                else if (type.EndsWith(";") || type.Equals(";"))
+                {
+                    return true;
+                }
+                else
+                { return false; }
+            }
+
+            public bool HasExpression(string type)
+            {
+                if (type.Contains("="))
+                { return true; }
+                else
+                { return false; }
+            }
+
+            public bool HasOperator(string type)
+            {
+                if (type.Equals("+") || type.Equals("-") || type.Equals("/") || type.Equals("*"))
+                { return true; }
+                else
+                { return false; }
+            }
+
+            public bool HasTERM(string type)
+            {
+                if (type.Equals("int") || type.Equals("string") || type.Equals("double") || type.Equals("float") || type.Equals("char") ||
+                    type.Equals("+") || type.Equals("-") || type.Equals("/") || type.Equals("*") || type.Equals("void") ||
+                    type.Equals("main") || type.Equals("scanf") || type.Equals("printf") || type.Equals("=") || type.Equals("(") || type.Equals(")") ||
+                    type.Equals(";") || type.StartsWith("\"") || type.EndsWith("\"") || type.EndsWith(";"))
+                {
+                    return false;
+                }
+                else
+                { return true; }
+            }
+
+            public bool HasKeyword(string type)
+            {
+                if(type.Equals("scanf") || type.Equals("printf") || type.Equals("main"))
+                { return true; }
+                else
+                { return false; }
+            }
+
+            public bool HasStringLiteral(string type)
+            {
+                if(type.StartsWith("\"") || type.EndsWith("\""))
+                { return true; }
+                else
+                { return false; }
+            }
+           
+            public bool HasLbrace(string type)
+            {
+                if (type.StartsWith("{") || type.Equals("{"))
+                { return true; }
+                else
+                { return false; }
+            }
+
+            public bool HasRbrace(string type)
+            {
+                if (type.StartsWith("}") || type.Equals("}"))
+                { return true; }
+                else
+                { return false; }
+            }
+
+            public bool HasLscope(string type)
+            {
+                if(type.StartsWith("(") || type.Equals("("))
+                { return true; }
+                else
+                { return false; }
+            }
+
+            public bool HasRscope(string type)
+            {
+                if (type.EndsWith(")") || type.Equals(")"))
+                { return true; }
+                else
+                { return false; }
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txt = richTextBox1.Text;
+            txt = txt.Replace(" ", "\n");
+            txt = txt.Replace("(", "(\n");
+            txt = txt.Replace(";", "\n;");
+            richTextBox4.Text = txt;
+            string[] str = richTextBox4.Lines;
+            LinkedList<string> getList = new LinkedList<string>();
+            List<string> errorList = new List<string>();
+            VarDeclareCheck check = new VarDeclareCheck();
+            int j = 2;
+            int i = 0;
+            int cBrace = 0;
+            try
+            {
+                do
+                {
+                    //Basic Variable
+                    if (check.HasDtype(str[i]))
+                    {
+                        getList.AddLast(str[i]);
+                        if(check.HasTERM(str[i + 1]))
+                        {
+                            getList.AddLast(str[i + 1]);
+                            if(check.HasSemi(str[i + 2]))
+                            {
+                                getList.AddLast(str[i + 2]);
+                                i = i + 3;
+                            }
+                            //Basic var declare with expression
+                            else if (check.HasExpression(str[i + 2]))
+                            {
+                                getList.AddLast(str[i + 2]);
+                                if (check.HasStringLiteral(str[i + 3]))
+                                {
+                                    getList.AddLast(str[i + 3]);
+                                    if(check.HasSemi(str[i + 4]))
+                                    {
+                                        getList.AddLast(str[i + 4]);
+                                        i = i + 5;
+                                    }
+                                    else
+                                    { errorList.Add(str[i + 4]);
+                                        richTextBox3.AppendText("\n Missing ;");
+                                        break; }
+                                }
+                                else if (check.HasTERM(str[i + 3]) || check.HasStringLiteral(str[i + 3]))
+                                {
+                                    getList.AddLast(str[i + 3]);
+                                    if (check.HasOperator(str[i + 4]))
+                                    {
+                                        getList.AddLast(str[i + 4]);
+                                        if (check.HasTERM(str[i + 5]))
+                                        {
+                                            getList.AddLast(str[i + 5]);
+                                            i = i + 6;
+                                        }
+                                        else if (check.HasSemi(str[i + 5]) || check.HasStringLiteral(str[i + 5]))
+                                        {
+                                            getList.AddLast(str[i + 5]);
+                                            i = i + 6;
+                                        }
+                                        else
+                                        { errorList.Add(str[i + 5]);
+                                            richTextBox3.AppendText("\n Syntax Error");
+                                            break; }
+                                    }
+                                    else
+                                    { errorList.Add(str[i + 4]);
+                                        richTextBox3.AppendText("\n Missing Operator");
+                                        break; }
+                                }
+                                else
+                                { errorList.Add(str[i + 3]);
+                                    richTextBox3.AppendText("\n Syntax Error");
+                                    break; }
+                            }
+                            else
+                            { errorList.Add(str[i + 2]);
+                                richTextBox3.AppendText("\n Missing Expression");
+                                break; }
+                        }
+                        else
+                        { errorList.Add(str[i + 1]);
+                            richTextBox3.AppendText("\n Missing identifier");
+                            break; }
+                    }
+
+                    //printf & scanf
+                    else if(check.HasKeyword(str[i]))
+                    {
+                        getList.AddLast(str[i]);
+                        if (check.HasLscope(str[i + 1]))
+                        {
+                            getList.AddLast(str[i + 1]);
+                            if(check.HasStringLiteral(str[i + j]) || check.HasRscope(str[i + j]))
+                            {
+                                getList.AddLast(str[i + j]);
+                                j = j + 1;
+                                if (check.HasSemi(str[i + j]))
+                                {
+                                    getList.AddLast(str[i + j]);
+                                    i = i + 4;
+                                }
+                                else if(check.HasStringLiteral(str[i + j]) || check.HasRscope(str[i + j]))
+                                {
+                                    getList.AddLast(str[i + j]);
+                                    j = j + 1;
+                                    if (check.HasSemi(str[i + j]))
+                                    {
+                                        getList.AddLast(str[i + j]);
+                                        i = i + 5;
+                                    }
+                                }
+                                else
+                                { errorList.Add(str[i + j]);
+                                    richTextBox3.AppendText("\n Syntax Error");
+                                    break; }
+                            }
+                            else
+                            { errorList.Add(str[i + j]);
+                                richTextBox3.AppendText("\n Syntax Error");
+                                break; }
+                        }
+                        else
+                        { errorList.Add(str[i + 1]);
+                            richTextBox3.AppendText("\n Missing (");
+                            break; }
+                    }
+
+                    //Function Declare (void)
+                    else if (str[i] == "void")
+                    {
+                        getList.AddLast(str[i]);
+                        if (check.HasKeyword(str[i + 1]) || check.HasTERM(str[i + 1]))
+                        {
+                            getList.AddLast(str[i + 1]);
+                            if (check.HasLscope(str[i + 2]))
+                            {
+                                getList.AddLast(str[i + 2]);
+                                if(check.HasRscope(str[i + 3]))
+                                {
+                                    getList.AddLast(str[i + 3]);
+                                    i = i + 4;
+
+                                    if (check.HasRbrace(str[i]))
+                                    {
+                                        cBrace++;
+                                        if (cBrace % 2 == 0)
+                                        {
+                                            getList.AddLast(str[i]);
+                                            i = i + 1;
+                                        }
+                                        else if (cBrace == 1)
+                                        {
+                                            richTextBox3.AppendText("\n Missing { brace");
+                                            break;
+                                        }
+                                        else
+                                        {
+                                            richTextBox3.AppendText("\n Missing { brace");
+                                            break;
+                                        }
+                                    }
+
+                                    else if (check.HasLbrace(str[i]))
+                                    {
+                                        cBrace++;
+                                        if (cBrace % 2 == 0)
+                                        {
+                                            getList.AddLast(str[i]);
+                                            i = i + 1;
+                                        }
+                                        else if (cBrace == 1)
+                                        {
+                                            richTextBox3.AppendText("\n Missing } brace");
+                                            break;
+                                        }
+                                        else
+                                        {
+                                            richTextBox3.AppendText("\n Missing } brace");
+                                            break;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        richTextBox3.AppendText("\n Missing { } brace");
+                                        break;
+                                    }
+
+                                }
+                                else
+                                { errorList.Add(str[i + 3]);
+                                    richTextBox3.AppendText("\n Missing )");
+                                    break; }
+                            }
+                            else
+                            { errorList.Add(str[i + 2]);
+                                richTextBox3.AppendText("\n Missing (");
+                                break; }
+                        }
+                        else
+                        { errorList.Add(str[i + 1]);
+                            richTextBox3.AppendText("\n Missing keyword");
+                            break; }
+                    }
+
+                    else
+                    { errorList.Add(str[i]); break; }
+
+                } while (i <= str.Length);
+            }
+            catch
+            {
+                 
+            }
+                
+            foreach(var item in getList)
+            {
+                richTextBox2.AppendText(item + " ");
+            }
+
+            foreach (var item in errorList)
+            {
+                richTextBox3.AppendText(item + " ");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            richTextBox2.Text = "";
+            richTextBox3.Text = "";
+            richTextBox4.Text = "";
+        }
+    }
+}
